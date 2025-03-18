426ab7a3063e11fae1ca4d4edb72992e50ab8d1a
Juan Carlos Blanco Delgado <juancarlosjr97@gmail.com>
2025-03-18 00:36:28 +0000

    Fix test for rendering App component and ensure heading is in the document
    #22

--- src/App.test.jsx
+++ src/App.test.jsx
@@ -4,12 +4,13 @@ import { render } from "vitest-browser-react";
 import App from "./App";
 
 test("renders name", async () => {
-  const { getByText, getByRole } = render(<App />);
+  const { getByText } = render(<App />);
 
   const headingTitle = "React + Vite + Supabase + Vercel";
 
   const headingElement = getByText(headingTitle);
 
-  expect(headingElement.element().innerHTML).toBe(headingTitle);
   await expect.element(headingElement).toBeInTheDocument();
+
+  expect(headingElement.element().innerHTML).toBe(headingTitle);
 });
